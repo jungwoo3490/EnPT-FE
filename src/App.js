dcf9f1b73bf22d81c25494d710a49b95c7ee176f
Hyein Choi <113275575+Hyein514@users.noreply.github.com>
2023-07-03 22:20:05 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
-import LevelSettingPage from "./pages/LevelSettingPage";
-import TopicSettingPage from "./pages/TopicSettingPage";
-import EndPage from "./pages/EndPage";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LevelSettingPage />} />
-          <Route path="/select" element={<TopicSettingPage />} />
-          <Route path="/end" element={<EndPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
