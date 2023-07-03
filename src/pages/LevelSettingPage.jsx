7054d57d2cf0cfbb7e8c9f54f8023c7871f4ec7f
jungwoo3490 <jungwoo3490@naver.com>
2023-07-03 18:52:19 +0900

    레벨 선택 페이지 초안 완성

--- src/pages/LevelSettingPage.jsx
+++ src/pages/LevelSettingPage.jsx
@@ -1,13 +1,22 @@
 import logoImage from '../assets/logo.png'
+import CategoriBtn from '../components/CategoriBtn';
+import LevelBtn from '../components/LevelBtn';
 import '../styles/LevelSettingPage.css'
 
 const LevelSettingPage = () => {
     return (
         <div className="LevelSettingPage">
             <img src={logoImage} alt="" className='logoimage'/>
+            <div className='testbox'>
+                <div style={{fontSize: '48px', fontWeight: "bold", height: "60px", lineHeight: "60px", marginTop: "143px"}}>대화 레벨을 선택해주세요</div>
+                <div style={{fontSize: '24px', fontWeight: "700", height: "25px", lineHeight: "25px", color: "#a6a6a6", marginTop: "11px", marginBottom: "75px"}}>선택한 대화 레벨에 따라 답변의 길이 수가 달라져요!</div>
+                <LevelBtn text={"Pre"} underText={"초급"} color={"white"}/>
+                <LevelBtn text={"Mid"} underText={"중급"} color={"white"}/>
+                <LevelBtn text={"High"} underText={"고급"} color={"white"}/>
+            </div>
         </div>
+        
     )
 };
 
 export default LevelSettingPage;
-
