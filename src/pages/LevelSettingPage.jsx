e472cdfe384305634cea9394703fd99ba0ea9890
jungwoo3490 <jungwoo3490@naver.com>
2023-07-03 17:27:35 +0900

    초기 폴더 설정

---
+++ src/pages/LevelSettingPage.jsx
@@ -0,0 +1,13 @@
+import logoImage from '../assets/logo.png'
+import '../styles/LevelSettingPage.css'
+
+const LevelSettingPage = () => {
+    return (
+        <div className="LevelSettingPage">
+            <img src={logoImage} alt="" className='logoimage'/>
+        </div>
+    )
+};
+
+export default LevelSettingPage;
+
